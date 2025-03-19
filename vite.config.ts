810523faf0cfdb4955c7ed0d43687846317dedfd
Edward O <edwardoreilly0906@gmail.com>
2025-03-19 00:24:08 -0500

    chore: add Render domain to allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   envPrefix: 'VITE_',
   server: {
     port: Number(process.env.PORT) || 3000,
-    host: true // Listen on all addresses
+    host: true, // Listen on all addresses
+    allowedHosts: [
+      'git-candidates-database.onrender.com',
+      '.onrender.com' // This will allow all subdomains on render.com
+    ]
   }
 });
